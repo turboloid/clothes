0d84a933dc3821a25667a3af0437a495b11a9958
turboloid <64475746+turboloid@users.noreply.github.com>
2020-05-07 09:58:29 +0300

    Add files via upload

---
+++ 224/cdn/js/geo/sk.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        sk: {
+            name: 'Slovensko',
+            code: 'sk',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'Napríklad, +421 123456789',
+            nameHelper: 'Vladimir Gordik',
+            nameError: 'Meno je povinné',
+            phoneError: 'Nesprávne zadaný počet',
+            countryError: 'Krajina je povinná',
+            isDefault: true
+        }
+    };
+})();
---
+++ 224/js/translater.js
@@ -0,0 +1,165 @@
+var new_lang = {
+	
+
+"lt0": " revolučný  prevrat<img alt=\"\" src=\"img/@2x/p1.png\"><span>vďaka ketóze!</span>",
+
+"lt1": "iba dnes!<span>zľava 50%</span>",
+
+"lt2": "nová cena:<span class=\"price_main\"></span>",
+
+"lt3": "pôvodná cena:<span class=\"price_old\"></span>",
+
+"lt4": "Do konca akcie zostáva:",
+
+"lt5": "Vyberte si krajinu:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+
+"lt6": "Vaše meno:<input name=\"name\" placeholder=\"Jana Macková\" required=\"\" type=\"text\">",
+
+"lt7": "Telefónne číslo:<input name=\"phone\" placeholder=\"+421 900 000 000\" required=\"\" type=\"text\">",
+
+"lt8": "objednať ihneď",
+
+"lt9": " objednať ihneď ",
+
+"lt10": "Bezpečné chudnutie<span>s pomocou ketózy</span>",
+
+"lt11": "<b>Ketóza – získavanie energie z tukov, nie z glukózy, ktorá vstupuje do tela pomocou sacharidov.</b> Ak telo prestane dostávať energiu z uhľohydrátov, po 7 až 14 dňoch vstúpi do stavu ketózy. Výsledkom je aktívna premena tukov na energiu.",
+
+"lt12": " Diéta zahŕňajúca vylúčenie sacharidov z diéty je však pre človeka ťažké tolerovať..<br><span style=\"color: #9f116b; font-weight: 700;\">KETO DIÉTA vám pomôže začať proces ketózy bez toho, aby ste sa sami trápili.</span> Komplex zabezpečí príjem ketónu a telies z vonkajšej strany a pomôže vám zbaviť sa prebytočného tuku.",
+
+"lt13": "VAŠA",
+
+"lt14": "КЕТО",
+
+"lt15": "DIÉTA",
+
+"lt16": "keto<span>diet</span>",
+
+"lt17": " Povinná zložka stravy.",
+
+"lt18": " Kapsuly spúšťajú ketózu v tele a premieňajú uložené tuky na energiu. <b> Zaručene schudnete každý deň.</b>",
+
+"lt19": "VÝHERNÁ KOMBINÁCIA<span class=\"big\">КЕТОЗ+</span><span class=\"h3\">keto<span>diet</span></span>",
+
+"lt20": "Dosiahnutie ketózy zvyčajne trvá 7 až 14 dní. Iba 1 kapsula KETO DIET tento čas skráti na 40 - 50 minút, počas ktorého sa začne proces premeny tukov na energiu a znižovanie telesného tuku. Stačí užívať <span> KETO DIET denne a schudnete až o 10 až 15 kg  mesačne!</span>",
+
+"lt21": "1 krok",
+
+"lt22": " Ketóza vám umožní spaľovať ťažko dosiahnuteľný tuk pri zachovaní svalovej hmoty. Znížením príjmu uhľohydrátov sa zabezpečí stabilizácia vašej hmotnosti a nevrátenie stratených kilogramov.",
+
+"lt23": "2 krok",
+
+"lt24": "Keto Diet spúšťa pôsobenie ketózy: urýchľuje metabolizmus, znižuje chuť do jedla a priamo útočí na vaše tukové zásoby!",
+
+"lt25": "objednať ihneď",
+
+"lt26": "<span class=\"big\"> VÝKONNÉ ZLOŽKY</span>V ZLOŽENÍ<span class=\"h3\">keto<span>diet</span></span>",
+
+"lt27": " Malinové ketóny ",
+
+"lt28": " Tieto účinky sú podobné ako ketónové telá produkované ľudským telom. Mimoriadne aktívny.<ul><li><span>Premieňajú</span>tuk na energiu</li><li><span>Urýchľujú</span>metabolizmus</li><li><span>Potláčajú</span>chuť do jedla</li></ul>",
+
+"lt29": "Beta sitosterol",
+
+"lt30": " Neutralizuje účinky nesprávneho stravovania<ul><li><span>Normalizujú</span>metabolizmus lipidov</li><li><span>Čistí krvné cievy</span>od «zlého» cholesterolu</li><li><span>Zlepšuje postavu,</span>bojuje s celulitídou</li></ul>",
+
+"lt31": "Koenzým Q10",
+
+"lt32": " Deficit koenzýmu Q10 je príčinou obezity v 78% prípadov <ul><li><span>Нormalizuje</span>výmenu látok</li><li><span>Znižuje</span>cholesterol</li><li><span>Zbavuje Vás to,</span>prejedania sa</li></ul>",
+
+"lt33": " L-karnitín ",
+
+"lt34": " Látka spojená s vitamínmi B sa syntetizuje v ľudskom tele <ul><li><span>Premieňa</span>tuk na energiu</li><li><span> Zmierňuje podráždenosť,</span>zlepšuje náladu</li><li><span>Zbavuje</span>depresie</li></ul>",
+
+"lt35": "Bromelaín",
+
+"lt36": " Proteín a enzým tráviaci tuk vylučovaný z dužiny a stonky ananásu <ul><li><span>Aktívne rozpúšťa</span> viscerálny tuk </li><li><span>Odstraňuje</span>opuchy</li><li><span>Zabraňuje</span> obezite vnútorných orgánov </li></ul>",
+
+"lt37": " Organický synefrín ",
+
+"lt38": " Je výrazne lipolytický, blokuje aktivitu alfa receptorov v tukových bunkách <ul><li><span> Bojuje s „perzistentným“ tukom, vrátane toho viscerálneho </span></li><li><span> Zvyšuje spotrebu </span> telesných kalórií </li><li><span>Zvyšuje práceschopnosťь</span>a výdrž</li></ul>",
+
+"lt39": "NÁZOR ODBRNÍKOV NA<span class=\"h3\">keto<span>diet</span></span>",
+
+"lt40": "<span> Dorothy Goodall </span>, je americký odborník na výživu, v tomto odbore má prax 30 rokov.",
+
+"lt41": "Pri procese ketózy sa tuky rozdeľujú na mastné kyseliny a glycerín, ktoré sa následne transformujú na ketónové telá. Ketóza sa však spustí iba v prípade úplného vyčerpania zásob glykogénu v pečeni a svalovom tkanive, zvyčajne k tomu dôjde do 7-14 dní.</b>",
+
+"lt42": "Pre urýchlenie tohto procesu, a teda aj spaľovanie tukov, odporúčam svojim pacientom výživový doplnok KETO DIÉTY. KETO DIÉTY dokonale podporuje telo počas diéty keto: obohacuje potrebné prvky, potláča hlad, odstraňuje únavu a úzkosť..<b>KETO DIET, poskytuje zdravé a prirodzené chudnutie a nemenej dôležité, bez ochabnutej pokožky, ktorá vášmu telu prinesie mimoriadne výhody.</b>",
+
+"lt43": " Hlavnými vlastnosťami KETO DIÉTY sú urýchlenie prechodu tela do stavu ketózy a pomoc pri premene tukov na energiu..",
+
+"lt44": "objednať ihneď",
+
+"lt45": "Pripojte sa<b>k tisícke ľudí,</b>ktorí už vyskúšali<span class=\"h3\">keto<span>diet</span></span>",
+
+"lt46": "Recenzie",
+
+"lt47": "Previous",
+
+"lt50": "« Vždy som bola na diéte. Problémy s hladom, spánkom a kožou boli mojimi stálymi spoločníkmi. Na váhe bolo mínus, ale potom zas som sa začala prejedať a tak neustále dookola. O KETO DIET som sa dozvedela od odborníka na výživu. Výsledok chudnutia ma potešil takmer okamžite. Každý týždeň som videla na váhach mínus 3 kg. Pre mňa to bola skutočná mágia. Moja kondícia bola skvelá, netrpela som hladom a spala som 7 hodín».",
+
+"lt51": "Zuzana, 27 rokov",
+
+"lt52": "«Pre mňa sa KETO DIET stala záchranou. Nikdy som si nemyslela, že môžem jesť všetko, čo milujem a zároveň chudnúť. Faktom je, že som mala v strave vždy nadbytok uhľohydrátov. To malo za následok nadváhu 100 kg. Všeobecne, hneď ako som mierne upravil množstvo uhľohydrátov, hmotnosť začala klesať. Ale nemôžem žiť bez uhľohydrátov, pretože mám veľmi rada rýchle občerstvenie. Potom som sa dozvedel o KETO DIET. Po užívaní tohto lieku proces horenia prebytku prešiel oveľa rýchlejšie a ja som sa ani nemusela vzdať uhľohydrátov. Celkovo som za 4 mesiace schudla 25 kg. KETO DIET je jednoducho super! Odporúčam ho každému!»",
+
+"lt53": "Monika, 49 rokov",
+
+"lt54": "Ako málo vieme o našom tele. Nemyslela som ani na to, aké zložité procesy v ňom prebiehajú, až kým som sa nedostala k výžive a nepočula o ketóze. Som lenivý človek, neviem sedieť na diétach, ani športovať. Preto som hľadala niečo ľahké a rýchle. O KETO DIET môžem povedať jednu vec a to, že ju musíte vyskúšať! S ňou som schudla 15 kg a dosiahla tak svoju ideálnu postavu.",
+
+"lt55": "Jarmila, 34 rokov",
+
+"lt62": "Next",
+
+"lt63": "Prišiel čas<b>sa zbaviť komplexov</b>",
+
+"lt64": " revolučný  prevrat <img alt=\"\" src=\"img/@2x/p1.png\"><span>vďaka ketóze!</span>",
+
+"lt65": "iba dnes!<span>zľava 50%</span>",
+
+"lt66": "nová cena:<span class=\"price_main\"></span>",
+
+"lt67": "pôvodná cena:<span class=\"price_old\"></span>",
+
+"lt68": "Do konca akcie ostáva:",
+
+"lt69": "Vyberte si krajinu:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+
+"lt70": "Vaše meno:<input name=\"name\" placeholder=\" Jana Macková \" required=\"\" type=\"text\">",
+
+"lt71": "Telefónne číslo:<input name=\"phone\" placeholder=\"+421900 000 000\" required=\"\" type=\"text\">",
+
+"lt72": "objednať ihneď",
+
+"lt73": "Ako pracujeme",
+
+"lt74": " OBJEDNÁVANIE ",
+
+"lt75": "Ihneď po dokončení objednávky, vás bude náš manažér kontaktovať <br><b> a potvrdí objednávku </b>",
+
+"lt76": "DODANIE",
+
+"lt77": " Ihneď po dokončení objednávky, vás bude náš manažér kontaktovať <br><b> a potvrdí objednávku </b>",
+"lt78": "PLATBA",
+
+"lt79": "<b> Žiadne platby vopred!</b> Platba objednávok sa vykonáva striktne pri prevzatí tovaru.",
+
+"lt80": "Záruka",
+
+"lt81": " Pri nákupe v našom internetovom obchode si môžete byť istí vysokou kvalitou tovaru ",
+
+  
+
+
+	
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
